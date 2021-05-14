a4bcd8b87f235d07f7194cca55e8e6b57e8dd7d6
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-05-14 12:38:33 +0430

    Add log-out button to "messenger window"

--- src/dibgram/messenger/messengerWindow.js
+++ src/dibgram/messenger/messengerWindow.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import TdLib from '../TdWeb/tdlib';
+import BigHighlightedButton from '../ui/elements/highlighted-button';
 
 export function MessengerWindow () {
+    function logOut() {
+        TdLib.sendQuery({
+            '@type': 'logOut'
+        });
+    }
     return (
-        <div>This is messenger window. You&apos;ve authenticated successfully!</div>
+        <div>
+            This is messenger window. You&apos;ve authenticated successfully!
+            <BigHighlightedButton onClick={logOut}>Log out</BigHighlightedButton>
+        </div>
     );
 }
