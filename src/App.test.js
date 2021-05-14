53d12509b42ef15045c5e0a43594c1ca454cd6fa
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-05-15 01:06:39 +0430

    Delete App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('see if app runs', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Loading.../i);
-    expect(linkElement).toBeInTheDocument();
-});
