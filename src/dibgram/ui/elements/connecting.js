0780c62ded51eb2d8083d096205f5c0906b72a53
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-05-15 18:27:13 +0430

    Move loading spinner to a separate component
    It will surely be used again somewhere else.

--- src/dibgram/ui/elements/connecting.js
+++ src/dibgram/ui/elements/connecting.js
@@ -1,24 +1,18 @@
-import { ProgressRing } from 'progress-ring';
 import React from 'react';
 import TdLib from '../../TdWeb/tdlib';
+import LoadingSpinner from './loading-spinner';
 
 export default class ConnectionState extends React.Component {
     state= {
         cState: 'connectionStateReady',
-        percentage: 10
     }
     render () {
         return (
             <div className="connectionState">
-                <ProgressRing
-                    percent={this.state.percentage}
-                    caps='round'
-                    trackColor="transparent"
+                <LoadingSpinner
                     progressColor="gray"
                     size="20"
-                    lineWidth="2"
-                    spin="true"
-                    transitionDuration="2000"/>
+                    lineWidth="2"/>
             </div>
         );
     }
@@ -29,15 +23,9 @@ export default class ConnectionState extends React.Component {
     }
     componentDidMount () {
         TdLib.registerUpdateHandler('updateConnectionState', this.handleUpdate);
-        this.intervalNum= setInterval(() => {
-            this.setState({
-                percentage: (90 - this.state.percentage)
-            });
-        }, 2000);
     }
     componentWillUnmount () {
         TdLib.registerUpdateHandler('updateConnectionState', this.handleUpdate);
-        clearInterval(this.intervalNum);
     }
 
 }
---
+++ src/dibgram/ui/elements/loading-spinner.js
@@ -0,0 +1,40 @@
+import { ProgressRing } from 'progress-ring';
+import 'progress-ring/src/styles.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class LoadingSpinner extends React.Component {
+    static props= {
+        size: PropTypes.number.isRequired,
+        lineWidth: PropTypes.number.isRequired,
+        progressColor: PropTypes.string.isRequired
+    }
+    state= {
+        percentage: 10
+    }
+    render () {
+        return (
+            <div className="loading-spinner">
+                <ProgressRing
+                    percent={this.state.percentage}
+                    caps='round'
+                    trackColor="transparent"
+                    spin="true"
+                    {...this.props}
+                    transitionDuration="2000"/>
+            </div>
+        );
+    }
+
+    componentDidMount () {
+        this.intervalNum= setInterval(() => {
+            this.setState({
+                percentage: (90 - this.state.percentage)
+            });
+        }, 2000);
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.intervalNum);
+    }
+}
