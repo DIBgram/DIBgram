2da94a2435198bebd8cfcecde7c1ccbaa5d1a839
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-05-16 19:03:40 +0430

    Add component navigation-menu (UI currently empty)

---
+++ src/dibgram/messenger/ui/navigation-menu.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect, Provider } from 'react-redux';
+import chatFiltersStore from '../chat-filters';
+import ChatFolderList from './chat-folders';
+
+
+const NavMenu = connect(function (state) {
+    return {useFolders: (state||[]).length!=0};
+})(function NavMenu(props) {
+    if(props.useFolders) {
+        return (
+            <div id="navigation-menu" className="uses-folders">
+                <ChatFolderList/>
+                <div id="chat-list-container">
+                    
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div id="navigation-menu">
+
+            </div>
+        );
+    }
+});
+
+export default function NavigationMenu() {
+    return (
+        <Provider store={chatFiltersStore}>
+            <NavMenu/>
+        </Provider>
+    );
+}
