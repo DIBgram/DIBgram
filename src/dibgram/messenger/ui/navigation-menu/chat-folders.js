0c3664474160c7373fd9b084148c65b293119e8e
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-07-23 13:25:30 +0430

    Add chat folders list (but no click handlers or icons)

--- src/dibgram/messenger/ui/navigation-menu/chat-folders.js
+++ src/dibgram/messenger/ui/navigation-menu/chat-folders.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import filters from '../../../ui/icon/chat_filters/chat-filters';
+
+/**
+ * Renders a chat folder button
+ */
+function ChatFolder(props) {
+    return (
+        <li className={props.active ? 'active' : ''}>
+            <div className="icon" dangerouslySetInnerHTML={{__html:filters.all[props.active+0]}}></div>
+            <div className="title">{props.folder.title}</div>
+        </li>
+    );
+}
+ChatFolder.propTypes= {
+    /** The folder object provided by TdLib */
+    folder: PropTypes.object,
+    /** A boolean indicating if the folder is currently selected */
+    active: PropTypes.bool
+};
+export {ChatFolder};
 
 /**
  * Renders the chat folders list
  */
-export default function ChatFolderList() {
+function ChatFolderList(props) {
     return (
         <ul>
-
+            <ChatFolder active={true} folder={{
+                title: 'All chats',
+                icon: 'all'
+            }}/>
+            {props.folders.map(folder=><ChatFolder folder={folder} active={false} key={folder.id}/>)}
         </ul>
     );
 }
+ChatFolderList.propTypes={
+    folders: PropTypes.arrayOf(PropTypes.object)
+};
+export default connect(state=>({folders:state}))(ChatFolderList);
---
+++ src/dibgram/ui/icon/chat_filters/chat-filters.js
@@ -0,0 +1,8 @@
+// All icons are from Telegram Desktop, converted to SVG using https://pngtosvg.com/
+
+import raw from 'raw.macro';
+
+var chatFilerIcons = {
+    all: [raw('./all.svg'), raw('./all_active.svg')]
+};
+export default chatFilerIcons;
