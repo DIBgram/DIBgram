e652c764afdda8c8546c7d8bcdf24b11dd30af67
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-07-31 16:15:31 +0430

    Ripple effect on light buttons

--- src/dibgram/ui/elements/small-button.js
+++ src/dibgram/ui/elements/small-button.js
@@ -1,20 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RippleEffect from './ripple-effect';
 
 /**
  * Renders a small button with accent color
  */
-export default function SmallButton(props) {
-    return (
-        <button 
-            className="small-button" 
-            {...props}>
+export default class SmallButton extends React.Component {
+    static propTypes= {
+        /** The button's content */
+        children: PropTypes.node.isRequired,
+        /** Fires when the button is clicked */
+        onClick: PropTypes.func
+    };
+    state= {
+        ripple: {
+            state: 'off'
+        }
+    }
+    /**@param e {React.SyntheticEvent} */
+    mouseDown= (e) => {
+        this.setState({
+            ripple: {
+                state: 'pressed',
+                X: e.nativeEvent.offsetX,
+                Y: e.nativeEvent.offsetY,
+                width: e.target.clientWidth,
+                height: e.target.clientHeight
+            }
+        });
+    }
+    mouseUp= () => {
+        this.setState({
+            ripple: { state: 'released' }
+        });
+        setTimeout(() => {
+            if(this.state.ripple.state=='released'){
+                this.setState({
+                    ripple: { state: 'off' }
+                });
+            }
+        }, 1000);
+    }
+    mouseLeave= () => {
+        if(this.state.ripple.state=='pressed') {
+            this.mouseUp();
+        }
+    }
+    render() {
+        return (
+            <button 
+                className="small-button" 
+                onClick={this.props.onClick}
+                onMouseDown={this.mouseDown}
+                onMouseUp={this.mouseUp}
+                onMouseLeave={this.mouseLeave}>
 
-            {props.children}
-            
-        </button>
-    );
+                <RippleEffect {...this.state.ripple} color="var(--theme-color-lightButtonBgRipple)"/>
+
+                <div className="invisibleText">
+                    {this.props.children}
+                </div>
+                <div className="content">
+                    {this.props.children}
+                </div>
+                
+            </button>
+        );
+    }
 }
-SmallButton.propTypes= {
-    children: PropTypes.node.isRequired
-};
