7dc21c0cd8c80cd7772e4556aace105ecf7d5dd2
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-08-01 14:29:48 +0430

    Add ripple effect to chat folder button

--- src/dibgram/messenger/ui/navigation-menu/chat-folders.js
+++ src/dibgram/messenger/ui/navigation-menu/chat-folders.js
@@ -2,17 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import filters from '../../../ui/icon/chat_filters/chat-filters';
+import RippleEffect, {handleMyMouseEvents} from '../../../ui/elements/ripple-effect';
 
 /**
  * Renders a chat folder button
  */
-function ChatFolder(props) {
-    return (
-        <li className={props.active ? 'active' : ''}>
-            <div className="icon" dangerouslySetInnerHTML={{__html:filters.all[props.active+0]}}></div>
-            <div className="title">{props.folder.title}</div>
-        </li>
-    );
+class ChatFolder extends React.Component {
+    state= {
+        ripple: {
+            state: 'off'
+        }
+    }
+    constructor() {
+        super();
+        [this.mouseDown, this.mouseUp, this.mouseLeave]= handleMyMouseEvents(this);
+    }
+    render(){
+        return (
+            <li className={this.props.active ? 'active' : ''}>
+                <RippleEffect {...this.state.ripple} color="var(--theme-color-sideBarBgRipple)"/>
+                <button
+                    onMouseDown={this.mouseDown}
+                    onMouseUp={this.mouseUp}
+                    onMouseLeave={this.mouseLeave}>
+                    <div className="icon" dangerouslySetInnerHTML={{__html:filters.all[this.props.active+0]}}></div>
+                    <div className="title">{this.props.folder.title}</div>
+                </button>
+            </li>
+        );
+    }
 }
 ChatFolder.propTypes= {
     /** The folder object provided by TdLib */
