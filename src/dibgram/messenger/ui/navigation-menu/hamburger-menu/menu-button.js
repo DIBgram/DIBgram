5d00e67d6fea839152560d4b5501e78aa9b8fc64
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-08-01 15:00:51 +0430

    Add ripple effect to hamburger menu button

--- src/dibgram/messenger/ui/navigation-menu/hamburger-menu/menu-button.js
+++ src/dibgram/messenger/ui/navigation-menu/hamburger-menu/menu-button.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import RippleEffect, {handleMyMouseEvents} from '../../../../ui/elements/ripple-effect';
 /**
  * Renders the button which opens the hamburger menu
  */
-export default function HamburgerMenuButton() {
-    return (
-        <button id="hamburger-menu-button">
-            <span></span><span></span><span></span>
-        </button>
-    );
+export default class HamburgerMenuButton extends React.Component {
+    state= {
+        ripple: {
+            state: 'off'
+        }
+    }
+    constructor() {
+        super();
+        [this.mouseDown, this.mouseUp, this.mouseLeave]= handleMyMouseEvents(this);
+    }
+    render() {
+        return (
+            <button id="hamburger-menu-button"
+                onMouseDown={this.mouseDown}
+                onMouseUp={this.mouseUp}
+                onMouseLeave={this.mouseLeave}>
+                <div id="bars"><span></span><span></span><span></span></div>
+                <RippleEffect {...this.state.ripple} color="var(--theme-color-sideBarBgRipple)"/>
+            </button>
+        );
+    }
 }
