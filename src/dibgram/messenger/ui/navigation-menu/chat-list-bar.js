86dbfbc417b492681019d5cbb97469820ee8a0d4
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-08-08 16:45:05 +0430

    Implement search box (no handling code)

--- src/dibgram/messenger/ui/navigation-menu/chat-list-bar.js
+++ src/dibgram/messenger/ui/navigation-menu/chat-list-bar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ConnectionState from '../../../ui/components/connecting';
+import SearchBox from './search/search-box';
 
 /**
  * Renders the navigation menu, containing chat list, search field and search results, chat folders and the hamburger menu
@@ -10,6 +11,7 @@ const ChatListBar = connect(function (state) {
 })(function ChatListBar() {
     return (
         <div id="chat-list-bar">
+            <SearchBox/>
             <ConnectionState/>
         </div>
     );
---
+++ src/dibgram/messenger/ui/navigation-menu/search/search-box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search-box.scss';
+
+/**
+ * Renders the search box.
+ */
+export default function SearchBox() {
+    return (
+        <div id="search-box">
+            <input placeholder="Search"/>
+        </div>
+    );
+}
