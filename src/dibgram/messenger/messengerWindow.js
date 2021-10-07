48b8f8567c95743ad997f805b1f869a24f113a9d
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-10-07 23:25:33 +0330

    Allow resizing chat list

--- src/dibgram/messenger/messengerWindow.js
+++ src/dibgram/messenger/messengerWindow.js
@@ -5,6 +5,7 @@ import chatStore from './chat-store';
 import { Provider } from 'react-redux';
 import './messengerWindow.scss';
 import HamburgerMenu from './sections/navigation-menu/hamburger-menu/menu';
+import {Resizable} from 're-resizable';
 
 /**
  * Renders the messenger screen
@@ -18,7 +19,11 @@ export function MessengerWindow () {
             <Provider store={chatStore}>
                 <HamburgerMenu visible={isMenuOpen} onClose={()=> setIsMenuOpen(false)}/>
                 <ChatFoldersList onHamburgerMenuOpened={()=> setIsMenuOpen(true)}/>
-                <ChatListBar onHamburgerMenuOpened={()=> setIsMenuOpen(true)}/>
+                <Resizable defaultSize={{width: 260}} minWidth={260}
+                    className="chat-list-bar-container" handleClasses={{right: 'chat-list-bar-resize-handle'}}
+                    enable={{top: false, right: true, bottom: false, left: false, topRight: false, bottomRight: false, bottomLeft: false, topLeft: false}}>
+                    <ChatListBar onHamburgerMenuOpened={()=> setIsMenuOpen(true)}/>
+                </Resizable>
             </Provider>
             <div id="chat-container">
                 
