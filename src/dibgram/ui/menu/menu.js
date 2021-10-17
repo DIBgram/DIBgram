2fa6d13ca99458bb8621b6a32bb489777914c5c2
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-10-17 11:07:04 +0330

    Add ripple effect to context menu

--- src/dibgram/ui/menu/menu.js
+++ src/dibgram/ui/menu/menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './menu.scss';
+import RippleEffect, { handleMyMouseEventsFunction } from '../elements/ripple-effect';
 
 /**
  * A dropdown menu (not to be confused with `<select>`).
@@ -24,9 +25,14 @@ Menu.MenuContents= function MenuContents(props) {
  * A menu item.
  */
 Menu.MenuItem= function MenuItem({children, ...rest}) {
+    const ripple= React.useState({state: 'off'});
+    const [mouseDown, mouseUp, mouseLeave] = handleMyMouseEventsFunction(ripple);
     return (
-        <div className="menu-item" {...rest}>
-            {children}
+        <div className="menu-item" {...rest} onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseLeave={mouseLeave}>
+            <RippleEffect {...ripple[0]} color="var(--theme-color-windowBgRipple)"/>
+            <div className="content">
+                {children}
+            </div>
         </div>
     );
 };
