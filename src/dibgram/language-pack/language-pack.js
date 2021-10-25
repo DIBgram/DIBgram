e34a212944b6f9a0b705053ded406b4683466c59
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-10-25 14:41:16 +0330

    Fix incomplete commit (?)

--- src/dibgram/language-pack/language-pack.js
+++ src/dibgram/language-pack/language-pack.js
@@ -14,24 +14,7 @@ export function initLanguagePack(){
             value: 'tdesktop'
         }
     }).then(()=>{
-        let languageInfo= localStorage.getItem('dibgram-active-language');
-        if(languageInfo) languageInfo= JSON.parse(languageInfo);
-        if(!languageInfo) languageInfo= {
-            '@type': 'languagePackInfo',
-            base_language_pack_id: '',
-            id: 'en',
-            is_beta: false,
-            is_installed: false,
-            is_official: true,
-            is_rtl: false,
-            local_string_count: 2784,
-            name: 'English',
-            native_name: 'English',
-            plural_code: 'en',
-            total_string_count: 2784,
-            translated_string_count: 2784,
-            translation_url: 'https://translations.telegram.org/en/'
-        };
+        const languageInfo= getCurrentLanguagePack();
         TdLib.sendQuery({
             '@type': 'getLanguagePackStrings',
             language_pack_id: languageInfo.id || 'en'
@@ -47,6 +30,28 @@ export function initLanguagePack(){
             });
         });
     });
+}
+
+export function getCurrentLanguagePack() {
+    let languageInfo= localStorage.getItem('dibgram-active-language');
+    if(languageInfo) languageInfo= JSON.parse(languageInfo);
+    if(!languageInfo) languageInfo= {
+        '@type': 'languagePackInfo',
+        base_language_pack_id: '',
+        id: 'en',
+        is_beta: false,
+        is_installed: false,
+        is_official: true,
+        is_rtl: false,
+        local_string_count: 2784,
+        name: 'English',
+        native_name: 'English',
+        plural_code: 'en',
+        total_string_count: 2784,
+        translated_string_count: 2784,
+        translation_url: 'https://translations.telegram.org/en/'
+    };
+    return languageInfo;
 }
 
 function getFormattedText(text){
--- src/dibgram/messenger/sections/settings/choose-lanugage.js
+++ src/dibgram/messenger/sections/settings/choose-lanugage.js
@@ -5,11 +5,12 @@ import SmallButton from '../../../ui/elements/small-button';
 import ScrollView from '../../../ui/scroll/scrollbar';
 import RippleEffect, { handleMyMouseEventsFunction } from '../../../ui/elements/ripple-effect';
 import BoxSearch from '../../../ui/dialog/search';
-import __ from '../../../language-pack/language-pack';
+import __, { getCurrentLanguagePack } from '../../../language-pack/language-pack';
 import TdLib from '../../../TdWeb/tdlib';
 import './choose-language.scss';
 import { addDialog } from '../../../ui/dialog/dialogs';
 import ConfirmDialog from '../../../ui/dialog/confirm-dialog';
+import ToolStrip from '../../../ui/tool-strip/tool-strip';
 
 export default function ChooseLanguageDialog({id}) {
     const ref = React.useRef();
@@ -28,6 +29,8 @@ export default function ChooseLanguageDialog({id}) {
         });
     }, []);
 
+    const currentPack= getCurrentLanguagePack();
+
     return (
         <Dialog ref={ref} id={id} width="320px" className="confirm-dialog">
             <h1>{__('lng_languages')}</h1>
@@ -35,30 +38,15 @@ export default function ChooseLanguageDialog({id}) {
             <BoxSearch value={search} onChange={(e) => setSearch(e.target.value)} />
 
             <ScrollView scrollAlwaysVisible>
-                {languages.map(pack => (
-                    <LanguagePack key={pack.id} pack={pack} onClick={ ()=>{
-                        TdLib.sendQuery({
-                            '@type': 'getLanguagePackStrings',
-                            language_pack_id: pack.id,
-                            keys: [ 'lng_sure_save_language' ]
-                        }).then(response => {
-                            addDialog('settings-language-restart-confirm-dialog', (
-                                <ConfirmDialog largeFont={true} onOK={() => {
-                                    localStorage.setItem('dibgram-active-language', JSON.stringify(pack));
-                                    window.location.reload();
-                                }} id="settings-language-restart-confirm-dialog">
-                                    {__('lng_sure_save_language')}<br/>
-                                    <br/>
-                                    {response.strings[0].value.value}
-                                </ConfirmDialog>
-                            ));
-                            TdLib.sendQuery({ // Request language pack strings so it is already cached after restart
-                                '@type': 'getLanguagePackStrings',
-                                language_pack_id: pack.id,
-                            });
-                        });
-                    }}/>
-                ))}
+                <ToolStrip.Section>
+                    {languages.map(pack => (
+                        <LanguagePack 
+                            key={pack.id} 
+                            pack={pack} 
+                            onClick={()=> selectLanguage(pack)} 
+                            selected={currentPack.id == pack.id}/>
+                    ))}
+                </ToolStrip.Section>
             </ScrollView>
 
             <div className="options">
@@ -88,3 +76,26 @@ LanguagePack.propTypes = {
     pack: PropTypes.object.isRequired,
     onClick: PropTypes.func,
 };
+
+function selectLanguage(pack) {
+    TdLib.sendQuery({
+        '@type': 'getLanguagePackStrings',
+        language_pack_id: pack.id,
+        keys: [ 'lng_sure_save_language' ]
+    }).then(response => {
+        addDialog('settings-language-restart-confirm-dialog', (
+            <ConfirmDialog largeFont={true} onOK={() => {
+                localStorage.setItem('dibgram-active-language', JSON.stringify(pack));
+                window.location.reload();
+            }} id="settings-language-restart-confirm-dialog">
+                {__('lng_sure_save_language')}<br/>
+                <br/>
+                {response.strings[0].value.value}
+            </ConfirmDialog>
+        ));
+        TdLib.sendQuery({ // Request language pack strings so it is already cached after restart
+            '@type': 'getLanguagePackStrings',
+            language_pack_id: pack.id,
+        });
+    });
+}
