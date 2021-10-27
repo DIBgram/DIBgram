f5b1a5027d6a7edc759e0a750fcb38173acc38a9
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-10-27 10:41:49 +0330

    Fix incomplete commit (?)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import {authStore, MainApp} from './dibgram/auth/auth-screen';
-import { getCurrentLanguagePack, initLanguagePack } from './dibgram/language-pack/language-pack';
+import { initLanguagePack } from './dibgram/language-pack/language-pack';
 import setInitialOnlineStatus from './dibgram/TdWeb/online-handler';
 import TdLib from './dibgram/TdWeb/tdlib';
 import ConfirmDialog from './dibgram/ui/dialog/confirm-dialog';
@@ -53,7 +53,7 @@ function App() {
 
     return (
         <Provider store={themeStore}>
-            <ThemeProvider id="app" dir={(getCurrentLanguagePack(false)?.is_rtl || false) ? 'rtl' : 'ltr'}>
+            <ThemeProvider id="app">
                 <Provider store={toastStore}>
                     <Toasts/>
                 </Provider>
