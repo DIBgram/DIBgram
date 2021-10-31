c837034c470e3e6ab40ea04d823665634b1eb612
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-10-31 11:53:09 +0330

    Move `messenger/payments` to `dibgram/sections/payments`

--- src/dibgram/messenger/message/message-summary-noicon.js
+++ src/dibgram/messenger/message/message-summary-noicon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TdLib from '../../TdWeb/tdlib';
-import currencyAmountToString from '../payments/currency-tostring';
+import currencyAmountToString from '../sections/payments/currency-tostring';
 import {getUserFullName} from '../user-misc';
 import { getChatNoCache } from '../chat-store';
 import MessageShortName from './message-pinned-message';
