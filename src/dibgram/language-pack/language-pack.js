fc79cbb52581fc5a813f83bbdfc28f0e9c817b20
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-11-11 14:33:29 +0330

    Localization: Split language pack parser and string formatter

--- src/dibgram/language-pack/language-pack.js
+++ src/dibgram/language-pack/language-pack.js
@@ -1,11 +1,9 @@
-import React from 'react';
 import { authStore } from '../auth/auth-screen';
 import TdLib from '../TdWeb/tdlib';
 import englishLanguagePack from './english.json';
+import { applyKeys, formatString, getCountMode, getFormattedText, getPluralString } from './string-format';
 
 var currentLanguagePack= null;
-
-const applyKeys= (item, index) => (<React.Fragment key={index}>{item}</React.Fragment>);
 
 export function initLanguagePack(){
     TdLib.sendQuery({
@@ -63,66 +61,6 @@ export function getCurrentLanguagePack(englishIsDefault= true) {
 
 export function getRtlMode() {
     return (localStorage.getItem('dibgram-allow-rtl-layout') == 'true') && (getCurrentLanguagePack(false)?.is_rtl || false);
-}
-
-function getFormattedText(text){
-    const formatted= applyFormatting(text);
-    if(formatted.length === 1) return formatted[0];
-    return formatted.map(applyKeys);
-}
-
-function applyFormatting(format) {
-    var res= [];
-    /* eslint-disable-next-line no-constant-condition */
-    while(true) {
-        if(format.includes('\n')){ // New line
-            const index= format.indexOf('\n');
-            applyFormatting(format.substr(0, index)).forEach(e=>res.push(e));
-            res.push(<br/>);
-            format= format.substr(index + 1);
-        } 
-        else if(format.includes('**')){ // Bold
-            const execed= /\*\*(.+)\*\*/.exec(format);
-            applyFormatting(format.substr(0, execed.index)).forEach(e=>res.push(e));
-            res.push(<strong>{execed[1]}</strong>);
-            format= format.substr(execed.index + execed[1].length+4);
-        }
-        else break;
-    }
-    res.push(format);
-    return res;
-}
-
-export function formatString(format, params= {}) {
-    var res= [];
-    while(format.includes('{')){
-        const execed= /{(\w+)}/.exec(format);
-        applyFormatting(format.substr(0, execed.index)).forEach(e=>res.push(e));
-        res.push(params[execed[1]]);
-        format= format.substr(execed.index + execed[1].length+2);
-    }
-    return [...res, ...applyFormatting(format)];
-}
-
-// Returns 'zero', 'one', 'two', 'few', 'many' or 'other' depending on the number
-function getCountMode(count) {
-    if(count === 0) return 'zero';
-    if(count === 1) return 'one';
-    if(count === 2) return 'two';
-    if(count > 2 && count < 10) return 'few';
-    if(count >= 10) return 'many';
-    return 'other';
-}
-
-function getPluralString(mode, callback) {
-    switch(mode) {
-    case 'zero': return callback('zero') || callback('few') || callback('many') || callback('other');
-    case 'one': return callback('one') || callback('other');
-    case 'two': return callback('two') || callback('few') || callback('many') || callback('other');
-    case 'few': return callback('few') || callback('many') || callback('other');
-    case 'many': return callback('many') || callback('other');
-    case 'other': return callback('other');
-    }
 }
 
 /**
