f7197e1fee760844fd7b8d0909fe7dabc9b3e909
Muaath Alqarni <muaath1428@hotmail.com>
2021-11-16 10:02:00 +0300

    Better & Localized three dots menu item depends on chat type

--- src/dibgram/messenger/sections/chat/chat-section.js
+++ src/dibgram/messenger/sections/chat/chat-section.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import __ from '../../../language-pack/language-pack';
+import __, { __pl } from '../../../language-pack/language-pack';
 import IconButton from '../../../ui/elements/icon-button';
 import './chat-section.scss';
 import ThreeDotsMenu from '../../../ui/menu/three-dots-menu';
 import Menu from '../../../ui/menu/menu';
 import { settings_advenced } from '../../../ui/icon/icons';
+import options from '../../../TdWeb/options';
+import TdLib from '../../../TdWeb/tdlib';
 
 export const ChatSection= 
 connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSection({chats, selectedChat}) {
@@ -18,10 +20,12 @@ connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSectio
     }
     if(!chat) return (
         <div id="chat-container">
-            {/* Needs to be in the middle, I don't know how */}
-            <span className="service-message">
-                {__('lng_willbe_history')}
-            </span>
+            {/* Needs to be in the middle vertically, I don't know how */}
+            <div className="middle">
+                <span className="service-message">
+                    {__('lng_willbe_history')}
+                </span>
+            </div>
         </div>
     );
 
@@ -29,27 +33,34 @@ connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSectio
         <div id="chat-container">
             <div id="headers">
                 <div id="title-bar">
-                    {/*TODO: Show 'Saved Messages' if it was self*/}
-                    {chat.title}
+                    {(chat.id === options.my_id) ? __('lng_saved_messages') : chat.title}
 
-                    {/*TODO: Use right theme color*/}
                     <div id="info">
-                        {/* Needs translation & depends on chat type*/}
-                        {5} members
+                        {(chat.type['@type'] == 'chatTypeSupergroup' && chat.type.is_channel) && (__pl('lng_chat_status_subscribers', 5))}
+                        {(chat.type['@type'] == 'chatTypeSupergroup' && !chat.type.is_channel || chat.type['@type'] == 'chatTypeBasicGroup') && (__pl('lng_chat_status_members', 5))}
                     </div>
                 </div>
 
-                {/*I don't know how to place these buttons in the right place*/}
+                {/* I don't know how to place these buttons in the right place */}
                 <div id="icon-buttons">
-                    {/* Needs icons: voice_chat, search, And third_column_view */}
+                    {/* Needed icons: voice_chat if exists, search, And third_column_view */}
                     <IconButton icon={settings_advenced} />
                     <IconButton icon={settings_advenced} />
                     <ThreeDotsMenu>
                         <Menu.MenuContents>
-                            {/* Needs to be translated & depends on chat type */}
-                            <Menu.MenuItem>View chat info</Menu.MenuItem>
-                            <Menu.MenuItem>Leave chat</Menu.MenuItem>
-                            <Menu.MenuItem>Report</Menu.MenuItem>
+                            {/* Channel menu items */}
+                            {(chat.type['@type'] == 'chatTypeSupergroup' && chat.type.is_channel) && (<Menu.MenuItem>{__('lng_context_view_channel')}</Menu.MenuItem>)}
+                            {(chat.type['@type'] == 'chatTypeSupergroup' && chat.type.is_channel) && ((<Menu.MenuItem>{__('lng_profile_leave_channel')}</Menu.MenuItem>))}
+                            
+                            {/* Group menu items */}
+                            {((chat.type['@type'] === 'chatTypeSupergroup' && !chat.type.is_channel) || chat.type['@type'] === 'chatTypeBasicGroup') &&
+                                (<Menu.MenuItem>{__('lng_context_view_group')}</Menu.MenuItem>)}
+                            {((chat.type['@type'] === 'chatTypeSupergroup' && !chat.type.is_channel) || chat.type['@type'] === 'chatTypeBasicGroup') &&
+                                (<Menu.MenuItem>{__('lng_profile_leave_group')}</Menu.MenuItem>)}
+                            {((chat.type['@type'] === 'chatTypeSupergroup' && !chat.type.is_channel) || chat.type['@type'] === 'chatTypeBasicGroup') &&
+                                (<Menu.MenuItem>{__('lng_profile_clear_history')}</Menu.MenuItem>)}
+
+                            {chat.can_be_reported && (<Menu.MenuItem>{__('lng_report_button')}</Menu.MenuItem>)}
                         </Menu.MenuContents>
                     </ThreeDotsMenu>
                 </div>
