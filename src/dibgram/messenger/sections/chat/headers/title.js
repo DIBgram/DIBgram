f1e5554576200b87462fd27f996927b6050e9819
Muaath Alqarni <muaath1428@hotmail.com>
2021-11-16 14:23:38 +0300

    Add ThreeDotsButton & Subscribers count

--- src/dibgram/messenger/sections/chat/headers/title.js
+++ src/dibgram/messenger/sections/chat/headers/title.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './title.scss';
+import __, { __pl } from '../../../../language-pack/language-pack';
+import ThreeDotsMenu from '../../../../ui/menu/three-dots-menu';
+import Menu from '../../../../ui/menu/menu';
 
 export default function TitleHeader({chat}) {
     return (
@@ -9,6 +12,17 @@ export default function TitleHeader({chat}) {
                 <div className="title">
                     {chat.title}
                 </div>
+                <div className="info">
+                    {(chat.type['@type'] == 'chatTypeSupergroup' && chat.type.is_channel) && __pl('lng_chat_status_subscribers', 5)}
+                    {(chat.type['@type'] == 'chatTypeBasicGroup' || (chat.type['@type'] == 'chatTypeSupergroup' && !chat.type.is_channel)) && __pl('lng_chat_status_members', 5)}
+                </div>
+            </div>
+            <div className="title-bar-right">
+                <ThreeDotsMenu>
+                    <Menu.MenuContents>
+                        <Menu.MenuItem>{chat.can_be_reported && __('lng_report_button')}</Menu.MenuItem>
+                    </Menu.MenuContents>
+                </ThreeDotsMenu>
             </div>
         </div>
     );
