402be96c83445399d68553a8da44312c0cb654f7
Muaath Alqarni <muaath1428@hotmail.com>
2021-11-16 10:14:51 +0300

    Add subscribers/members count

--- src/dibgram/messenger/sections/chat/headers/title.js
+++ src/dibgram/messenger/sections/chat/headers/title.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './title.scss';
-
+import { __pl } from '../../../../language-pack/language-pack';
 export default function TitleHeader({chat}) {
     return (
         <div className="title-bar">
             <div className="title-bar-left">
                 <div className="title">
                     {chat.title}
+                </div>
+                <div className="info">
+                    {/* TODO: Add real subscribers/members count, Not 5 all the time */}
+                    {(chat.type['@type'] == 'chatTypeSupergroup' && chat.type.is_channel) && (__pl('lng_chat_status_subscribers', 5))}
+                    {(chat.type['@type'] == 'chatTypeSupergroup' && !chat.type.is_channel || chat.type['@type'] == 'chatTypeBasicGroup') && (__pl('lng_chat_status_members', 5))}
                 </div>
             </div>
         </div>
