3b969e8fb9a32e99278b8b6623e5498dad9a1752
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-11-17 18:35:59 +0330

    Use a better method to get type-specific chat info

--- src/dibgram/messenger/sections/chat/chat-section.js
+++ src/dibgram/messenger/sections/chat/chat-section.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import __ from '../../../language-pack/language-pack';
 import { ServiceMessage } from '../../message/ui/message-containers';
 import TitleHeader from './headers/title';
 import './chat-section.scss';
-import TdLib from '../../../TdWeb/tdlib';
+import { ChatHistory } from './history/history';
+import usersStore from '../../users-store';
+import supergroupStore from '../../supergroup-store';
+import basicGroupStore from '../../basic-group-store';
 
 export const ChatSection= 
 connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSection({chats, selectedChat}) {
@@ -15,6 +18,7 @@ connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSectio
             break;
         }
     }
+
     if(!chat) return (
         <div id="chat-section" className="no-chat">
             <ServiceMessage>
@@ -22,25 +26,52 @@ connect(({chats, selectedChat}) => ({chats, selectedChat})) (function ChatSectio
             </ServiceMessage>
         </div>
     );
+    switch(chat.type['@type']){
+    case 'chatTypePrivate':
+        return (
+            <Provider store={usersStore}>
+                <ChatSectionContentWrapperPrivate chat={chat}/>
+            </Provider>
+        );
+    case 'chatTypeBasicGroup': 
+        return (
+            <Provider store={basicGroupStore}>
+                <ChatSectionContentWrapperBasicGroup chat={chat}/>
+            </Provider>
+        );
+    case 'chatTypeSupergroup':
+        return (
+            <Provider store={supergroupStore}>
+                <ChatSectionContentWrapperSupergroup chat={chat}/>
+            </Provider>
+        );
+    }
+});
 
-    let chatFull;
-    React.use
-    if (chat.type['@type'] == 'chatTypeSupergroup') {
-        TdLib.sendQuery({'@type': 'getSupergroupFullInfo', 'supergroup_id': chat.type.supergroup_id}).then((supergroupFull) => {
-            chatFull = supergroupFull;
-        });
+const ChatSectionContentWrapperPrivate= connect(users=>({users}))(
+    function ChatSectionContentWrapperPrivate({users, chat}) {
+        return <ChatSectionContentWrapper user={users[chat.type.user_id]} chat={chat}/>;
     }
-    if (chat.type['@type'] == 'chatTypeBasicGroup') {
-        TdLib.sendQuery({'@type': 'getBasicGroupFullInfo', 'basic_group_id': chat.type.basic_group_id}).then((basicGroupFull) => {
-            chatFull = basicGroupFull;
-        });
+);
+
+const ChatSectionContentWrapperBasicGroup= connect(basicGroups=>({basicGroups}))(
+    function ChatSectionContentWrapperBasicGroup({basicGroups, chat}) {
+        return <ChatSectionContentWrapper basicGroup={basicGroups[chat.type.basic_group_id]} chat={chat}/>;
     }
-    
+);
+
+const ChatSectionContentWrapperSupergroup= connect(supergroups=>({supergroups}))(
+    function ChatSectionContentWrapperSupergroup({supergroups, chat}) {
+        return <ChatSectionContentWrapper supergroup={supergroups[chat.type.supergroup_id]} chat={chat}/>;
+    }
+);
+
+function ChatSectionContentWrapper(props) {
     return (
         <div id="chat-section">
             <div className="headers">
-                <TitleHeader chat={chat} chatFull={chatFull} />
+                <TitleHeader {...props}/>
             </div>
         </div>
     );
-});
+}
