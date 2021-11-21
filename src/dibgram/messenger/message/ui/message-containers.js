7b296cde9ecb2d3b31413bc0cbbb692d466a4474
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-11-21 13:07:58 +0330

    Show chat last message in chat history

--- src/dibgram/messenger/message/ui/message-containers.js
+++ src/dibgram/messenger/message/ui/message-containers.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { bubble_tail } from '../../../ui/icon/icons';
 
 import './message-containers.scss';
 
@@ -7,3 +9,16 @@ export function ServiceMessage(props) {
         <div className="history-service-message" {...props}/>
     );
 }
+
+export function MessageBubble({ children, showTail=true, ...rest}) {
+    return (
+        <div className={`bubble ${showTail ? 'has-tail' : ''}`} {...rest}>
+            {children}
+            {showTail && <span className="tail" dangerouslySetInnerHTML={{__html: bubble_tail}}/>}
+        </div>
+    );
+}
+MessageBubble.propTypes = {
+    children: PropTypes.node,
+    showTail: PropTypes.bool,
+};
--- src/dibgram/messenger/sections/chat/history/history.js
+++ src/dibgram/messenger/sections/chat/history/history.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './history.scss';
+import { MessageBubble } from '../../../message/ui/message-containers';
+import { _s__ } from '../../../../language-pack/language-pack';
 
-export function ChatHistory() {
+export function ChatHistory({chat}) {
+    const message= chat.last_message;
     return (
         <div className="history">
-
+            <div className={'history-message' + (message.is_outgoing ? ' outgoing' : '')}>
+                <MessageBubble>
+                    {message.content['@type'] === 'messageText' ?
+                        message.content.text.text :
+                        _s__('lngd_message_unsupported')}
+                </MessageBubble>
+            </div>
         </div>
     );
 }
+ChatHistory.propTypes = {
+    chat: PropTypes.object.isRequired
+};
