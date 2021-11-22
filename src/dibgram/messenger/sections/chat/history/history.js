25b57f7c5d6c51e226d8490f020571eb5254f39f
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-11-22 11:19:12 +0330

    Chat history can scroll

--- src/dibgram/messenger/sections/chat/history/history.js
+++ src/dibgram/messenger/sections/chat/history/history.js
@@ -4,23 +4,26 @@ import './history.scss';
 import { BubbleMessage } from '../../../message/ui/message-containers';
 import { _s__ } from '../../../../language-pack/language-pack';
 import withUsers from '../../../users-wrapper';
+import ScrollView from '../../../../ui/scroll/scrollbar';
 
 export function ChatHistory({chat}) {
     const BubbleMsg= withUsers(BubbleMessage);
 
     const message= chat.last_message;
     return (
-        <div className="history">
-            <div className={'history-message' + (message.is_outgoing ? ' outgoing' : '')}>
-                <BubbleMsg message={message} chat={chat}>
-                    <div className="content">
-                        {message.content['@type'] === 'messageText' ?
-                            message.content.text.text :
-                            _s__('lngd_message_unsupported')}
-                    </div>
-                </BubbleMsg>
+        <ScrollView className="history scrollbar" scrollBarWidth={6}>
+            <div className="history-scroll-content">
+                <div className={'history-message' + (message.is_outgoing ? ' outgoing' : '')}>
+                    <BubbleMsg message={message} chat={chat}>
+                        <div className="content">
+                            {message.content['@type'] === 'messageText' ?
+                                message.content.text.text :
+                                _s__('lngd_message_unsupported')}
+                        </div>
+                    </BubbleMsg>
+                </div>
             </div>
-        </div>
+        </ScrollView>
     );
 }
 ChatHistory.propTypes = {
