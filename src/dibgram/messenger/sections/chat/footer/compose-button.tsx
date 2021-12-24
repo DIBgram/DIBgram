e679a6b28015f7be957aeac07026f497c3cc9e14
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-12-24 13:24:55 +0330

    Add 'Open with' button to open a chat on alternate client
    DIBgram has many features missing and isn't convenient to use.
    This button allows users to open the chat on another client.

---
+++ src/dibgram/messenger/sections/chat/footer/compose-button.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import RippleEffect, { handleMyMouseEventsFunction } from '../../../../ui/elements/ripple-effect';
+import './compose-button.scss';
+
+type ComposeButtonProps = {
+    onClick: (e: React.SyntheticEvent<HTMLButtonElement>) => void;
+    children: React.ReactNode;
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    [key: string]: any;
+};
+
+export default function ComposeButton({ onClick, children, ...rest }: ComposeButtonProps): JSX.Element {
+    const ripple= React.useState({state: 'off'});
+    const [onMouseDown, onMouseUp, onMouseLeave]= handleMyMouseEventsFunction(ripple);
+    return (
+        <button className="compose-button" onClick={onClick} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onMouseLeave={onMouseLeave} {...rest}>
+            <RippleEffect {...ripple[0]} color="var(--theme-color-historyComposeButtonBgRipple"/>
+            <div className="content">{children}</div>
+        </button>
+    );
+}
