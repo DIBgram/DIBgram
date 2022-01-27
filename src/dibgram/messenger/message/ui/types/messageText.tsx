62ae3ca8a786f9e1669f6658b74ad8c2c3771f98
Mammad900 <mohammadhosseinabedi052@gmail.com>
2022-01-27 15:12:37 +0330

    Implement simple webpage preview

--- src/dibgram/messenger/message/ui/types/messageText.tsx
+++ src/dibgram/messenger/message/ui/types/messageText.tsx
@@ -4,6 +4,7 @@ import withUsers from '../../../users-wrapper';
 import compileEntities from '../entities';
 import { MessageProps } from '../message';
 import { BubbleMessage, MessageFooter } from '../message-containers';
+import './messageText.scss';
 
 export default function MessageText({message, chat}: MessageProps): JSX.Element {
     const messageContent= (message.content as TdApi.td_messageText);
@@ -11,10 +12,24 @@ export default function MessageText({message, chat}: MessageProps): JSX.Element 
     const BubbleMsg= withUsers(BubbleMessage);
     return (
         <BubbleMsg message={message} chat={chat}>
-            <div className={`content ${message.can_be_saved?'':'no-save'}`}>
+            <div className={`content text ${message.can_be_saved?'':'no-save'}`}>
                 {compileEntities(messageContent.text)}
+                {messageContent.web_page && <LinkPreview preview={messageContent.web_page} />}
                 <MessageFooter message={message} chat={chat}/>
             </div>
         </BubbleMsg>
+    );
+}
+
+type LinkPreviewProps = {
+    preview: TdApi.td_webPage;
+}
+function LinkPreview({preview}: LinkPreviewProps): JSX.Element {
+    return (
+        <div className="link-preview">
+            <div className="site-name">{preview.site_name}</div>
+            <div className="title">{preview.title}</div>
+            <div className="description">{compileEntities(preview.description)}</div>
+        </div>
     );
 }
